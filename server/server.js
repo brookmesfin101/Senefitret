ac84373aeab0abe94fec6c71d92f493ca483d2c1
Brook Mesfin <brook.mesfin@selerix.com>
2021-02-10 14:20:56 -0600

    added console log for port listening message

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
+const port = 8080;
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("<h1>Senefitret</h2>");
 });
 
-app.listen(8080);
+app.listen(port, () => {
+    console.log(`Senefitret app listening at port ${port}`);
+});
